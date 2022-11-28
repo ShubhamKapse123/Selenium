b1ba583f75353ee06f6644c04c49ad4d9e687edb
ShubhamKapse123 <skapse55555@gmail.com>
2022-11-28 12:36:18 +0530

    child parent traversal of element

---
+++ src/selenium/demo/Automation.java
@@ -0,0 +1,19 @@
+package selenium.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","G:\\drivers\\chromedriver.exe");
+		WebDriver wd= new ChromeDriver();
+		wd.get("https://rahulshettyacademy.com/AutomationPractice/");
+		//child traversal access element of child
+		System.out.println(wd.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		//parent traversal access child element 
+		System.out.println(wd.findElement(By.xpath("//header/div/button[1]/parent::div/button[1]")).getText());			
+	}
+
+}
